5833156eb856b0acbf0da7e9ee669fdba783154d
Jaison <jaisonschmidt@gmail.com>
2025-09-12 21:47:46 -0300

    style: implement localStorage for persistent light and dark mode settings

--- script.js
+++ script.js
@@ -2,10 +2,27 @@ const body = document.body;
 const lightBtn = document.getElementById("lightBtn");
 const darkBtn = document.getElementById("darkBtn");
 
+body.onload = function() {
+    // pegar o modo salvo no localstorage
+    const mode = localStorage.getItem("mode");
+    
+    // verificar se há um modo salvo
+    // se tiver um mode salvo, adiciona no body
+    if (mode) {
+        body.className = mode;
+        return;
+    }
+
+    // não tem um mode salvo, vamos salvar
+    localStorage.setItem("mode", "light");
+}
+
 lightBtn.onclick = function() {
     body.className = "light";
+    localStorage.setItem("mode", "light");
 }
 
 darkBtn.onclick = function() {
     body.className = "dark";
+    localStorage.setItem("mode", "dark");
 }
